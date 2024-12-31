534a5e9a800eb88aae0ca6e24a8a6ccbce3b3667
VanDung <trvdung2003@gmail.com>
2024-12-31 16:26:23 +0700

    k

--- BackEnd-main/auth/authRoutes.js
+++ BackEnd-main/auth/authRoutes.js
@@ -1,35 +1,34 @@
 const Account = require("../users/Account");
-const User= require("../users/User")
+const User = require("../users/User");
 
 async function authRoutes(fastify, opts) {
-
-  fastify.addHook('onReady', async () => {
-    const adminExists = await Account.findOne({ role: 'admin' });
+  fastify.addHook("onReady", async () => {
+    const adminExists = await Account.findOne({ role: "admin" });
     if (!adminExists) {
       const adminAccount = new Account({
-        email: 'admin@example.com',
-        password: 'admin123', // Use hashed password in production
-        role: 'admin'
+        email: "admin@example.com",
+        password: "admin123", // Use hashed password in production
+        role: "admin",
       });
       await adminAccount.save();
-      
+
       const adminUser = new User({
-        firstName: 'Admin',
-        lastName: 'User',
-        accountId: adminAccount._id
+        firstName: "Admin",
+        lastName: "User",
+        accountId: adminAccount._id,
       });
       await adminUser.save();
     }
   });
 
-  fastify.post('/register', async (req, rep) => {
+  fastify.post("/register", async (req, rep) => {
     try {
-      const { email, password, role, firstName, lastName} = req.body;
-  
+      const { email, password, role, firstName, lastName } = req.body;
+
       // Tạo tài khoản
       const account = new Account({ email, password, role });
       await account.save();
-  
+
       // Tạo user
       const user = new User({
         firstName,
@@ -37,46 +36,57 @@ async function authRoutes(fastify, opts) {
         accountId: account._id,
       });
       await user.save();
-  
-      rep.send({ message: 'Đăng ký thành công!' });
+
+      rep.send({ message: "Đăng ký thành công!" });
     } catch (err) {
-      rep.send({ error: 'Đăng kí thật bại', details: err });
+      rep.send({ error: "Đăng kí thật bại", details: err });
     }
   });
 
   // Route đăng nhập
-  fastify.post('/login', async (req, rep) => {
+  fastify.post("/login", async (req, rep) => {
     const { email, password } = req.body;
 
     try {
       const account = await Account.findOne({ email });
       if (!account || !account.verifyPassword(password)) {
-        return rep.send({ error: 'Mật khẩu hoặc Email không đúng' });
+        return rep.send({ error: "Mật khẩu hoặc Email không đúng" });
       }
 
       const user = await User.findOne({ accountId: account._id });
       if (!user) {
-        return rep.status(404).send({ error: 'Không tìm thấy thông tin người dùng' });
+        return rep
+          .status(404)
+          .send({ error: "Không tìm thấy thông tin người dùng" });
       }
 
-      const token = fastify.jwt.sign({ id: account._id, email: account.email, role: account.role }, { expiresIn: '1d' });
+      const token = fastify.jwt.sign(
+        { id: user._id, email: account.email, role: account.role },
+        { expiresIn: "1d" }
+      );
       rep
-        .setCookie('token', token, {
+        .setCookie("token", token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: 'Strict',
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "Strict",
         })
-        .send({ 
-          message: 'Đăng nhập thành công',
-          user: { id: account._id, firstName: user.firstName, lastName:user.lastName, role: account.role, token }
+        .send({
+          message: "Đăng nhập thành công",
+          user: {
+            id: account._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            role: account.role,
+            token,
+          },
         });
     } catch (err) {
-      rep.send({ error: 'Đăng nhập thất bại', details: err });
+      rep.send({ error: "Đăng nhập thất bại", details: err });
     }
   });
 
-  fastify.post('/logout', async (req, rep) => {
-    rep.clearCookie('token').send({ message: 'Logged out successfully' });
+  fastify.post("/logout", async (req, rep) => {
+    rep.clearCookie("token").send({ message: "Logged out successfully" });
   });
 }
 
